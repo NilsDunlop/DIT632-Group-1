113cddc1c9b46587912ce3ae47d03b2b2489aef4
NilsDunlop <gusdunlni@student.gu.se>
2022-01-19 10:41:09 +0100

    Add intial commit

---
+++ WP0/Exercise1/main.c
@@ -0,0 +1,10 @@
+// (C) Albert Simpson, Jihan Phanivong, Nils Dunlop group: 1 (2022)
+// Work package 0
+// Exercise 1
+// Submission code: XXXXXX (provided by your TA-s)
+#include <stdio.h>
+// Main function in the program, no program arguments supported
+void main (void) {
+    // Print a string to the console
+    printf("%s", "Hello World!\n");
+}
---
+++ WP0/Exercise2/main.c
@@ -0,0 +1,10 @@
+// (C) Albert Simpson, Jihan Phanivong, Nils Dunlop group: 1 (2022)
+// Work package 0
+// Exercise 2
+// Submission code: XXXXXX (provided by your TA-s)
+#include <stdio.h>
+// Main function in the program, supporting command line arguments
+void main (int argc, char* argv[]) {
+    // Print a string with the passed terminal argument to the console
+    printf("Hello World! - I'm %s!", argv[1]);
+}
